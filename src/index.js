e3a745ea5bfb592dd4f296fee0ad77fdea4c94f1
jeffpigg5@gmail.com <jeffpigg5@gmail.com>
2021-04-01 09:31:31 -0500

    Added function that whne you click on a square you will get an alert to your browser

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ registerServiceWorker();
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
+        <button className="square" onClick={function() { alert('click');
+}}>
           {this.props.value}
         </button>
       );
